34ba930f036507dbe508fb87a6836ffe45b59210
0BooM <149960425+0BooM@users.noreply.github.com>
2024-02-25 18:14:07 +0100

    Add .hover to styles.css, create addEventListener in scripts.js that add class hover every mousemove
    Added .hover class in styles.css and also made a forEach loop in scripts.js, to make our mouse draw on the canvas, by adding class hover to gridDiv every time mouse moves

--- scripts.js
+++ scripts.js
@@ -3,6 +3,13 @@ let containerDiv = document.querySelector(".container");
 for (let i = 0; i < (16 * 16); ++i) {
     let gridDiv = document.createElement('div');
     gridDiv.classList.add('grid-div');  
-    console.log(i);
     containerDiv.appendChild(gridDiv);
 }
+
+let gridDivs = document.querySelectorAll('.grid-div');
+
+gridDivs.forEach(gridDiv => {
+    gridDiv.addEventListener('mousemove', () => {
+        gridDiv.classList.add('hover');
+    })
+});
