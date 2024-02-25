a6d760a45ec4bc301df46d6ab2e2bb0f77533fa3
0BooM <149960425+0BooM@users.noreply.github.com>
2024-02-25 17:50:10 +0100

    Add .grid-div styles, make little changes in styles.css, create 16x16 canvas in script.js
    Added .grid-div styles and made little changes in * and .container in styles.css. Created canvas through iteration in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 let containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16; ++i) {}
+for (let i = 0; i < (16 * 16); ++i) {
+    let gridDiv = document.createElement('div');
+    gridDiv.classList.add('grid-div');  
+    console.log(i);
+    containerDiv.appendChild(gridDiv);
+}
