27682e0926553013b07dc78d17ec5ae1eb942b5a
0BooM <149960425+0BooM@users.noreply.github.com>
2024-02-28 20:37:45 +0100

    Add comments in scripts.js, and made makeNewGridRemoveTheOldOne() work correctly
    Made the function makeNewGridRemoveTheOldOne() correctly, by calculating new flex-basis and height in .grid-div to make it scale correctly after inputting new grid size

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// First Canvas
 let containerDiv = document.querySelector(".container");
 
 for (let i = 0; i < (16 * 16); ++i) {
@@ -5,14 +6,19 @@ for (let i = 0; i < (16 * 16); ++i) {
     gridDiv.classList.add('grid-div');  
     containerDiv.appendChild(gridDiv);
 }
+fillGridBackgroundByMovingMouse();
 
-let gridDivs = document.querySelectorAll('.grid-div');
+function fillGridBackgroundByMovingMouse(){
+    let gridDivs = document.querySelectorAll('.grid-div');
 
-gridDivs.forEach(gridDiv => {
-    gridDiv.addEventListener('mousemove', () => {
-        gridDiv.classList.add('hover');
-    })
-});
+    gridDivs.forEach(gridDiv => {
+        gridDiv.addEventListener('mousemove', () => {
+            gridDiv.classList.add('hover');
+        })
+    });
+}
+
+// Making canvas with user size
 
 let userValueBtn = document.querySelector('.userValue');
 
@@ -28,7 +34,24 @@ function askUserForAValue(){
             "How big do you want the grid to be? (Max number is 100)",
             "15");
         }
-        console.log(userGridValue);
 }
 
-userValueBtn.addEventListener('click', askUserForAValue)
+function makeNewGridRemoveTheOldOne(){
+    containerDiv.innerHTML = ''; // Remove content from canvas
+    let newGridValue = userGridValue * userGridValue
+    let gridDivFlexBasisValue = 960 / userGridValue;
+        for (let i = 0; i < newGridValue; ++i) {
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid-div");
+        gridDiv.style.flexBasis = gridDivFlexBasisValue + `px`;
+        gridDiv.style.height = gridDivFlexBasisValue + `px`;
+        containerDiv.appendChild(gridDiv);
+        }
+
+    fillGridBackgroundByMovingMouse();
+}
+
+userValueBtn.addEventListener('click', () => {
+    askUserForAValue();
+    makeNewGridRemoveTheOldOne();
+})
